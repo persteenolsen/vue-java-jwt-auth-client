a8e30a21193d42e79e9c21f92fb7fd3514a2309d
Per Olsen <persteen22@gmail.com>
2025-02-20 14:54:30 +0100

    Vue Client JWT Auth towards Java Spring Boot 3 API

---
+++ README.md
@@ -0,0 +1,44 @@
+# vue-3-pinia-jwt-auth
+
+Vue 3 + Pinia - JWT Authentication towards a Java Spring Boot 3 Web API
+
+# Last updated
+
+- 20-02-2025
+
+# Tech used for this Web Client
+
+- Node.js version 18.19.1
+- Volta for Node Version management
+- Vue 3 is the JS framework for this frontend
+- Pinia for state management
+- ESLint for analysing the .JS code
+- Vee-validate for validation
+- Yup for validation
+- vite v2.9.16 Dev Server
+- VS Code is my developement tool
+
+# Installing
+
+- Make sure you have a new version of Node installed
+- Download the code by zip or fork
+- Run the command npm install by the command promt
+
+# Development server:
+
+- npm run dev
+
+- You can view the development server at `localhost:3000`
+
+# Production build:
+
+- npm run build
+
+# Preview before Publish / Deployment
+
+- npm run preview
+
+- You can view the preview production built at `localhost:5050`
+
+
+
---
+++ src/views/LoginView.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { Form, Field } from 'vee-validate';
+import * as Yup from 'yup';
+
+import { useAuthStore } from '@/stores';
+
+const schema = Yup.object().shape({
+    username: Yup.string().required('Username is required'),
+    password: Yup.string().required('Password is required')
+});
+
+function onSubmit(values, { setErrors }) {
+    const authStore = useAuthStore();
+    const { username, password } = values;
+
+    return authStore.login(username, password)
+        .catch(error => setErrors({ apiError: error }));
+}
+</script>
+
+<template>
+    <div>
+        <h3>Vue 3 SPA + Java Spring Boot 3 Web API doing JWT Authentication</h3>
+        <br />Note: Due to the Free Azure App Service Plan, the Login process may take up to 1 minute:-)
+        <br/><br />
+        Last updated: 20-02-2025<br /><br />
+        <div class="alert alert-info">
+
+            Username: user<br />
+            Password: persteen1967
+        </div>
+        <h2>Login</h2>
+        <Form @submit="onSubmit" :validation-schema="schema" v-slot="{ errors, isSubmitting }">
+            <div class="form-group">
+                <label>Username</label>
+                <Field name="username" type="text" class="form-control" :class="{ 'is-invalid': errors.username }" />
+                <div class="invalid-feedback">{{errors.username}}</div>
+            </div>            
+            <div class="form-group">
+                <label>Password</label>
+                <Field name="password" type="password" class="form-control" :class="{ 'is-invalid': errors.password }" />
+                <div class="invalid-feedback">{{errors.password}}</div>
+            </div>            
+            <div class="form-group">
+                <button class="btn btn-primary" :disabled="isSubmitting">
+                    <span v-show="isSubmitting" class="spinner-border spinner-border-sm mr-1"></span>
+                    Login
+                </button>
+            </div>
+            <div v-if="errors.apiError" class="alert alert-danger mt-3 mb-0">{{errors.apiError}}</div>
+        </Form>
+    </div>
+</template>
