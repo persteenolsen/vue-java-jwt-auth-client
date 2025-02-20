d94947a2ccb9262501d03746a507d83c22ce7a96
Per Olsen <persteen22@gmail.com>
2025-02-20 20:58:04 +0100

    Updated error msg at Login View

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -47,7 +47,7 @@ function onSubmit(values, { setErrors }) {
                     Login
                 </button>
             </div>
-            <div v-if="errors.apiError" class="alert alert-danger mt-3 mb-0">Username or Password is incorrect ! Try again ...</div>
+            <div v-if="errors.apiError" class="alert alert-danger mt-3 mb-0">Access Denied! Try to enter Username and Password again...</div>
         </Form>
     </div>
 </template>
