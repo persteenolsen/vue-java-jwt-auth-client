70f07c4a8d8d4e1e7c3494109d48a9f9e3f74a62
Per Olsen <persteen22@gmail.com>
2025-02-21 08:12:23 +0100

    Updated the Login View

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -23,7 +23,7 @@ function onSubmit(values, { setErrors }) {
         <h3>Vue 3 SPA + Java Spring Boot 3 Web API doing JWT Authentication</h3>
         <br />Note: Due to the Free Azure App Service Plan, the Login process may take up to 1 minute:-)
         <br/><br />
-        Last updated: 20-02-2025<br /><br />
+        Last updated: 21-02-2025<br /><br />
         <div class="alert alert-info">
 
             Username: user<br />
